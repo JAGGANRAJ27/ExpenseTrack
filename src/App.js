1c60caa3fefbdaf7e4944ee4922aa2412598949a
jagganraj <jagganrajminion@gmail.com>
2023-03-08 19:51:14 +0530

    final commi001

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import { Grid } from '@material-ui/core';
-
-import { SpeechState, useSpeechContext } from "@speechly/react-client";
-import { PushToTalkButton, PushToTalkButtonContainer } from '@speechly/react-ui';
-
 import { Details, Main } from './components';
 import useStyles from './styles';
 
 const App = () => {
   const classes = useStyles();
-  const { speechState } = useSpeechContext();
   const main = useRef(null)
-
-  const executeScroll = () => main.current.scrollIntoView()    
-
-  useEffect(() => {
-    if (speechState === SpeechState.Recording) {
-      executeScroll();
-    }
-  }, [speechState]);
 
   return (
     <div>
-      <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh'}}>
+      <Grid className={classes.grid} container spacing={0} alignItems="center" justifyContent="center" style={{ height: '100vh'}}>
         <Grid item xs={12} sm={4} className={classes.mobile}>
           <Details title="Income" />
         </Grid>
@@ -35,9 +22,6 @@ const App = () => {
         <Grid item xs={12} sm={4} className={classes.last}>
           <Details title="Expense" />
         </Grid>
-        <PushToTalkButtonContainer>
-          <PushToTalkButton />
-        </PushToTalkButtonContainer>
       </Grid>
     </div>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,23 +1,20 @@
 import React, {  useContext } from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
-import { useSpeechContext } from '@speechly/react-client';
 import { ExpenseTrackerContext } from '../../context/context';
 import useStyles from './styles';
 import Form from './Form/Form';
 import List from './List/List';
-import InfoCard from '../InfoCard';
 
 const ExpenseTracker = () => {
   const classes = useStyles();
   const { balance } = useContext(ExpenseTrackerContext);
 
   return (
-    <Card className={classes.root}>
-      <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+    <Card className={classes.root} style={{background:'#C3F3C0'}}>
+      <CardHeader title="Expense Tracker" />
       <CardContent>
   <Typography align="center" variant="h5">Total Balance ₹{balance}</Typography>
         <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
-          <InfoCard />
         </Typography>
         <Divider className={classes.divider} />
         <Form />
